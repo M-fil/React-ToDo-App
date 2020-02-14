5de3bea3202c4e1c1e1ed5198d79c50505cfdabe
m_fil <filanovich.maxim11b@mail.ru>
2020-02-14 17:31:26 +0300

    init: start switch list view feature

---
+++ src/SwitchTasksListView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SwitchTasksListView() {
+    return (
+        <div>
+            <label htmlFor="switcher">Without Squares</label>
+            <input type="checkbox" id="switcher" />
+        </div>
+    )
+}
+
+export default SwitchTasksListView
