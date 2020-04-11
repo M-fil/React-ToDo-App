6a88aad78ff22c748c93de6e740c517ee8c34cf1
m_fil <filanovich.maxim11b@mail.ru>
2020-04-11 10:12:33 +0300

    Updates

---
+++ precache-manifest.8e380081ca52b9e0271bdb1e55e7b97a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a715fd66148548fc6e9f30141991ea32",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "178c19001c1ed363e0d7",
+    "url": "/React-ToDo-App/static/css/2.8705192a.chunk.css"
+  },
+  {
+    "revision": "34833865f824470a5909",
+    "url": "/React-ToDo-App/static/css/main.7831d72a.chunk.css"
+  },
+  {
+    "revision": "178c19001c1ed363e0d7",
+    "url": "/React-ToDo-App/static/js/2.aafab597.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React-ToDo-App/static/js/2.aafab597.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34833865f824470a5909",
+    "url": "/React-ToDo-App/static/js/main.29e115fd.chunk.js"
+  },
+  {
+    "revision": "8bd4c9bc8f946b1f3a48",
+    "url": "/React-ToDo-App/static/js/runtime-main.abf6ddcb.js"
+  },
+  {
+    "revision": "fa00a2091c3e3d21d17963856a753e2f",
+    "url": "/React-ToDo-App/static/media/checked.fa00a209.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-ToDo-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-ToDo-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-ToDo-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-ToDo-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-ToDo-App/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f546d57180e95a9f8b1f92cb4ec5331f",
+    "url": "/React-ToDo-App/static/media/image1.f546d571.jpg"
+  },
+  {
+    "revision": "a002c794d02cbbbd1182f9927c16109c",
+    "url": "/React-ToDo-App/static/media/image2.a002c794.jfif"
+  },
+  {
+    "revision": "ad2ad4ae567c2f1ae5cd1625ab0d5764",
+    "url": "/React-ToDo-App/static/media/image3.ad2ad4ae.jpg"
+  },
+  {
+    "revision": "5a722c82a5b4331ed1d57890f425be7a",
+    "url": "/React-ToDo-App/static/media/image4.5a722c82.jpg"
+  },
+  {
+    "revision": "989111069c7a7730b65ffaeb67e9c178",
+    "url": "/React-ToDo-App/static/media/image5.98911106.jpg"
+  },
+  {
+    "revision": "82eb2e461c20cd29539f0829a2c13967",
+    "url": "/React-ToDo-App/static/media/image6.82eb2e46.jpg"
+  }
+]);
